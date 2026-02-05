461c1c9f46fbcedd664c203609ff3a965640ca11
v4sud3v <iamcrv@gmail.com>
2026-02-05 19:50:29 +0530

    made a dummy calculator app for agent to work on

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action='store_true', help='Enable verbose output')
+    args = parser.parse_args()
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents = "Why is the sky blue?"
+        model="gemini-2.5-flash", contents = messages
     )
 
     if response is None or response.usage_metadata is None:
         print("No response received from the model.")
         return
     print(response.text)
-    print(f" Prompt Token {response.usage_metadata.prompt_token_count}")
-    print(f" Response Token {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f" Prompt Token {response.usage_metadata.prompt_token_count}")
+        print(f" Response Token {response.usage_metadata.candidates_token_count}")
 
 main()
